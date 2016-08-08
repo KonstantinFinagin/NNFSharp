041f4453174a65d6ca75759d2529014d55864203
Konstantin Finagin <konstantin.finagin@dataart.com>
2016-08-09 00:31:07 +0300

    feedforward completed, written SGD parameters

--- CharachterRecognition/Network/Network.fs
+++ CharachterRecognition/Network/Network.fs
@@ -4,26 +4,35 @@ open MathNet.Numerics.LinearAlgebra
 
 type Network(sizes : int[]) = 
 
-    let sigmoid z = 1.0 / (1.0 + exp z)
-
-
+    let sigmoid (z : Vector<double>) = z |> Vector.map(fun x -> 1.0 / (1.0 + exp x))
 
     member this.num_layers = sizes.Length
     member this.sizes = sizes
     member this.biases = [| for i in 1 .. sizes.Length-1
-                                do yield Matrix<double>.Build.Random(sizes.[i], 1)|]
+                                do yield Vector<double>.Build.Random(sizes.[i], 1)|]
     
     member this.weights = [| for i in 0 .. sizes.Length-2 do
                                 let x = sizes.[i]
                                 let y = sizes.[i+1]
                                 yield Matrix<double>.Build.Random(y, x) |]
 
+    member this.feedforward (a : Vector<double>) = 
+        let mutable result = a
+        for i in 0 .. this.num_layers-2 do
+            result <- sigmoid(this.weights.[i] * result + this.biases.[i])
+        result    
+
+    // Stochastic gradient descent
+    member this.SGD 
+        (training_data : (Vector<double> * Vector<double>)[]) 
+        (epochs : int) 
+        (mini_batch_size : int) 
+        (eta : double) 
+        (test_data : (Vector<double> * Vector<double>)[]) =
 
 
 
-
-
-
+        1
 
 
 
